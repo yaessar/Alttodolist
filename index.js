3cbfa7fab59440bc0e97616f1419a3b98586e9a3
yaessar <yagombat@gmail.com>
2022-09-02 17:21:55 +0300

    adding content to html through dom

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// index.js
-console.log("js is working today")
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      response.tasks.forEach(function (task) {
+        $('#todo-list').append('<p>' + task.content + '</p>');
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
+});
